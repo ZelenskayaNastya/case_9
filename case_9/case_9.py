3363af9cb7d5538d575d5f7efd3027edb1246194
MAXIMZOLOTYKH11 <max.zl@mail.ru>
2019-02-05 20:41:17 +0700

    commit1

---
+++ case_9/case_9.py
@@ -0,0 +1,31 @@
+# постоянные события
+def sell():
+    return
+
+
+def buy():
+    return
+
+
+def sow():
+    return
+
+
+def give():
+    return
+
+# неуправляемые события
+def one():
+    return
+
+
+# управляемые события
+def first():
+    return
+
+def main():
+    date = input()
+
+
+if __name__ == "__main__":
+    main()
